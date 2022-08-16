5263a182ba054f9fa54b2f0a4447d39b69ac8763
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-08-16 12:58:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SECCOMPLEX/SECAutoFilter.git /SECAutoFilter
+  git clone https://github.com/SECCOMPLEX/SECNETFLIXLK.git /SECNETFLIXLK
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /SECAutoFilter
+  git clone $UPSTREAM_REPO /SECNETFLIXLK
 fi
-cd /SECAutoFilter
+cd /SECNETFLIXLK
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
